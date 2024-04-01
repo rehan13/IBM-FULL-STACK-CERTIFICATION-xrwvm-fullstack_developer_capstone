42f986fe10e6f2392bd1cd24f6d6e7c2c03c1584
rehan <rehanmondal1312@gmail.com>
2024-04-01 18:31:19 -0400

    week5

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('register/', TemplateView.as_view(template_name="index.html")),
@@ -27,4 +30,5 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
